e184abcc4b1855cae3df6745074572cccec98b7e
elena.philipenko <elena.philipenko@teamdev.com>
2013-12-18 16:19:07 +0200

    Fix saving comments for existing markers

--- application/javascripts/controllers/map-front.js
+++ application/javascripts/controllers/map-front.js
@@ -1,7 +1,39 @@
 angular.module("map-front", ['map-back', 'comments-back', 'coordsForNewComment'])
     .controller("map-controller", ['$scope', '$location', '$http', '$timeout', '$log', 'Location', 'Coordinates', 'CommentMapped',
         function ($scope, $location, $http, $timeout, $log, Location, Coordinates, CommentMapped) {
+            function updateMarkers() {
+                CommentMapped.query(function (data) {
+                    $scope.map.markers = convertToMarkers(data);
+                    $scope.cursor = {
+                        latitude: $scope.map.latitude,
+                        longitude: $scope.map.longitude,
+                        title: "Your",
+                        onClicked: onMarkerClicked};
 
+                    $scope.map.markers.push($scope.cursor);
+                });
+            }
+
+            var onMarkerClicked = function () {
+                $location.path("/comment/" + this.latitude + "/" + this.longitude);
+                Coordinates.setCoords(this.latitude, this.longitude);
+                $scope.markerDetails.show();
+            };
+
+            function convertToMarkers(data) {
+                var markersWithComments = [];
+                var i = data.length - 1;
+                while (i >= 0) {
+                    var area = data[i];
+                    markersWithComments.push({
+                        latitude: area.coords.lat,
+                        longitude: area.coords.lng,
+                        title: "Comments: " + area.blogs,
+                        onClicked: onMarkerClicked});
+                    i--;
+                }
+                return markersWithComments;
+            }
 
             // Enable the new Google Maps visuals until it gets enabled by default.
             // See http://googlegeodevelopers.blogspot.ca/2013/05/a-fresh-new-look-for-maps-api-for-all.html
@@ -30,22 +62,7 @@ angular.module("map-front", ['map-back', 'comments-back', 'coordsForNewComment']
                     $scope.position.coords.longitude = $scope.location.lng;
                 }
             });
-            function convertToMarkers(data) {
-                var markersWithComments = [];
-                var i = data.length - 1;
-                while (i >= 0) {
-                    var area = data[i];
-                    markersWithComments.push({
-                        latitude: area.coords.lat,
-                        longitude: area.coords.lng,
-                        title: "Comments: " + area.blogs,
-                        onClicked: onMarkerClicked});
-                    i--;
-                }
-                return markersWithComments;
-            }
 
-            $scope.hideDetails = hideMarkerDetails;
             angular.extend($scope, {
                 map: {
                     center: {
@@ -76,29 +93,24 @@ angular.module("map-front", ['map-back', 'comments-back', 'coordsForNewComment']
                     }
                 }
             });
-            CommentMapped.query(function (data) {
-                $scope.map.markers = convertToMarkers(data);
-                $scope.cursor = {
-                    latitude: $scope.map.latitude,
-                    longitude: $scope.map.longitude,
-                    title: "Your",
-                    onClicked: onMarkerClicked};
-
-                $scope.map.markers.push($scope.cursor);
-            });
-
-            var onMarkerClicked = function () {
-                $location.path("/comment/" + this.latitude + "/" + this.longitude);
-                shoMarkerDetails();
-            };
+            $scope.markerDetails = new DetailsPopUp('#markerDetails', updateMarkers);
+            updateMarkers();
 
         }]
     );
-function hideMarkerDetails() {
-    $('#markerDetails').hide();
-}
-function shoMarkerDetails() {
-    $('#markerDetails').show();
-}
+
+var DetailsPopUp = function (selector, onClose) {
+    this.selector = selector;
+    this.onClose = onClose;
+};
+
+DetailsPopUp.prototype.show = function(){
+    $(this.selector).show();
+};
+
+DetailsPopUp.prototype.hide = function(){
+    $(this.selector).hide();
+    this.onClose();
+};
 
 
